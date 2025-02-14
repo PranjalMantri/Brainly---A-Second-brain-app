7e15868eb6b70f3fc0cdd55468ec55193c672039
Pranjal Mantri <pranjalmantri@gmail.com>
2025-02-14 13:06:29 +0530

    feat: add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,47 @@
+import { Schema, Document, model, CallbackError } from "mongoose";
+import bcrypt from "bcrypt";
+
+interface UserDocument extends Document {
+  username: string;
+  password: string;
+  comparePasswords(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<UserDocument>(
+  {
+    username: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error as CallbackError);
+  }
+});
+
+userSchema.methods.comparePasswords = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = model<UserDocument>("User", userSchema);
+
+export default User;
